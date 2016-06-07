028e967d8e00e2d408d6f1acb5e46e7c765f379a
Ahmed Aly <ahmaly01@gmail.com>
2016-06-07 16:07:49 -0400

    get images working with picasso

--- app/src/main/java/com/example/aaly/spotifind/ArtistInfoActivity.java
+++ app/src/main/java/com/example/aaly/spotifind/ArtistInfoActivity.java
@@ -3,9 +3,13 @@ package com.example.aaly.spotifind;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.aaly.spotifind.data.model.Item;
+import com.squareup.picasso.Picasso;
+
+import java.util.LinkedHashMap;
 
 
 public class ArtistInfoActivity extends AppCompatActivity {
@@ -15,9 +19,14 @@ public class ArtistInfoActivity extends AppCompatActivity {
         setContentView(R.layout.artist_info_layout);
         Intent activityThatCalled = getIntent();
         Item artist = (Item) activityThatCalled.getSerializableExtra("artist");
-
         TextView artistNameView = (TextView) findViewById(R.id.artistName);
-        if(artistNameView != null && artist !=null ) {
+        if (artist != null && artist.getImages().size() > 0) {
+            ImageView artistImageView = (ImageView) findViewById(R.id.artistImage);
+            Picasso.with(ArtistInfoActivity.this)
+                    .load(((LinkedHashMap) (artist.getImages().get(0))).get("url").toString())
+                    .into(artistImageView);
+        }
+        if (artistNameView != null && artist != null) {
             artistNameView.setText(artist.getName());
         }
     }
