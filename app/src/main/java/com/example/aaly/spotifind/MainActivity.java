9c978c8cbf91f360b0dc2ca47e2a233bd808d0a2
Ahmed Aly <ahmaly01@gmail.com>
2016-06-07 15:08:35 -0400

    reformat code

--- app/src/main/java/com/example/aaly/spotifind/MainActivity.java
+++ app/src/main/java/com/example/aaly/spotifind/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.aaly.spotifind;
 
 import android.content.Intent;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     EditText artistNameView;
     ProgressBar progressBarView;
     Button searchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         artistNameView = (EditText) findViewById(R.id.artistName);
         progressBarView = (ProgressBar) findViewById(R.id.progressBar);
         searchButton = (Button) findViewById(R.id.search);
-        searchButton.setOnClickListener( new View.OnClickListener(){
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SpotifyArtistAPI.Factory.getInstance().getArtists(artistNameView.getText().toString()).enqueue(new Callback<com.example.aaly.spotifind.data.model.Artist>() {
@@ -45,17 +45,17 @@ public class MainActivity extends AppCompatActivity {
 
                         final List<Item> artistList = response.body().getArtists().getItems();
                         ArtistAdapter artistAdapter = new ArtistAdapter(MainActivity.this, artistList);
-                        if(artistListView != null){
+                        if (artistListView != null) {
                             artistListView.setAdapter(artistAdapter);
                             artistListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                                 @Override
-                                public  void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
-                                    String artistPicked = "You selected " + String.valueOf(((Item)adapterView.getItemAtPosition(position)).getName());
+                                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                                    String artistPicked = "You selected " + String.valueOf(((Item) adapterView.getItemAtPosition(position)).getName());
                                     Toast.makeText(MainActivity.this, artistPicked, Toast.LENGTH_SHORT).show();
                                     Intent displayArtistInfoIntent = new Intent(MainActivity.this, ArtistInfoActivity.class);
                                     final int result = 1;
                                     Item item = artistList.get(position);
-                                    displayArtistInfoIntent.putExtra("artist", item );
+                                    displayArtistInfoIntent.putExtra("artist", item);
                                     startActivityForResult(displayArtistInfoIntent, result);
                                 }
                             });
